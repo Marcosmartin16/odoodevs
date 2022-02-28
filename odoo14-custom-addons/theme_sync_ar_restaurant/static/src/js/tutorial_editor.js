ab1495aa5dd44666cf99176ad5bed26cda02344d
Marcosmartin16 <91310797+Marcosmartin16@users.noreply.github.com>
2022-02-28 22:44:09 +0100

    Delete odoo14-custom-addons/theme_sync_ar_restaurant directory

--- odoo14-custom-addons/theme_sync_ar_restaurant/static/src/js/tutorial_editor.js
+++
@@ -1,8 +0,0 @@
-odoo.define(function (require) {
-    var options = require('web_editor.snippets.options');
-    options.registry.snippet_testimonial_options = options.Class.extend({
-        onFocus: function () {
-            alert("On focus!")
-        },
-    });
-});
